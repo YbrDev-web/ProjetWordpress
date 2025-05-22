a29288db38bbb4173163756071907951938c3aac
YbrDev-web <ybenromdhane2003@gmail.com>
2025-05-22 15:49:01 +0200

    contenu font

---
+++ SingleTheme/font-page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <?php include('Template-parts/identity_card.php'); ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?php get_footer() ?>
