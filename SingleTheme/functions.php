8a29b001ea3594f6ebdf82ed34527d263a4bf073
YbrDev-web <ybenromdhane2003@gmail.com>
2025-05-22 14:39:43 +0200

    ajout de la deuxième function

--- SingleTheme/functions.php
+++ SingleTheme/functions.php
@@ -7,4 +7,11 @@ function SingleTheme_register_nav_menu()
         'primary_menu' => __('Primary Menu', 'ProjetOpenclassroom'),
         'primary_menu' => __('Footer Menu', 'ProjetOpenclassroom'),
     ));
+
+    add_action('after_setup_theme', 'SingleTheme_add_theme_support', 0);
+    function SingleTheme_add_theme_support()
+    {
+        add_theme_support('custom-logo');
+        add_theme_support('post-thumbnails');
+    }
 }
