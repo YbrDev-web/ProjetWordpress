14a8ee59c3e462b943336f4907e75a2a205f40c5
YbrDev-web <ybenromdhane2003@gmail.com>
2025-05-22 15:44:12 +0200

    correction

--- SingleTheme/index.php
+++ SingleTheme/index.php
@@ -4,7 +4,7 @@
     <?php include('Template-parts/identity_card.php'); ?>
 
     <?php if (!is_front_page()) {
-        include('Template-parts/post-list.php');
+        include('Template-parts/post_list.php');
     }
     ?>
 </main>
